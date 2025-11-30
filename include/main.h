171ddee96fdc2dcc94dd3b4cd5f9c3cb55bfb53b
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2025-11-30 17:23:25 +0530

    Update main.h

--- include/main.h
+++ include/main.h
@@ -1,1 +1,17 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
 
+#define MAX_WORD_LENGTH 30
+#define MAX_GUESSES 6
+
+extern char word[MAX_WORD_LENGTH];
+extern char guessed[MAX_WORD_LENGTH];
+extern int wrongGuesses;
+extern char usedLetters[26];
+
+void printHangman();
+void printWord();
+int isGameOver();
+void playHangman();
+
+#endif
